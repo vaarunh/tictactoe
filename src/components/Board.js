4b7f7d6ba5293f57110bcd40a63c3ec8b6c451dc
Varun Herlekar <varunherlekar22@gmail.com>
2021-01-24 15:07:11 -0800

    added sass and styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,20 +4,22 @@ import Square from './Square';
 const Board = () => {
   return (
     <div>
-      <div>
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
-      </div>
-      <div>
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
-      </div>
-      <div>
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+      <div className="board">
+        <div className="board-row">
+          <Square value={0} />
+          <Square value={1} />
+          <Square value={2} />
+        </div>
+        <div className="board-row">
+          <Square value={3} />
+          <Square value={4} />
+          <Square value={5} />
+        </div>
+        <div className="board-row">
+          <Square value={6} />
+          <Square value={7} />
+          <Square value={8} />
+        </div>
       </div>
     </div>
   );
